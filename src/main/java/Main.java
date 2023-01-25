9f4995f15480ebb48f186424c07b6a43e1b4cccc
Simeon <simeonpopov94@gmail.com>
2023-01-25 07:32:35 +0200

    Deleted all test files and make a new configuration for test_DB but still don't know how to invoke the right DB in runtime. Refactored the SessionFactoryUtil to handle exceptions and to be able to load both configurations.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,17 +3,6 @@ import session.SessionFactoryUtil;
 
 public class Main {
     public static void main(String[] args) {
-//        Employee employee = new Employee(1,"Pesho", "Tihomirov");
-//
-//        EmployeeDAO.saveEmployee(employee);
-//        EmployeeDAO.getAllEmployees().forEach(System.out::println);
-//
-//        //try all function of CompanyDAO
-//        Company company = new Company(1,"SAP");
-//        CompanyDAO.saveCompany(company);
-//        CompanyDAO.deleteCompanyById(1);
-//        CompanyDAO.deleteCompanyById(2);
-//        CompanyDAO.getAllCompanies().forEach(System.out::println);
         UserInterface.getInstance().start();
     }
 }
--- src/main/java/session/SessionFactoryUtil.java
+++ src/main/java/session/SessionFactoryUtil.java
@@ -7,29 +7,38 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
 public class SessionFactoryUtil {
+    //TODO somehow this must be gotten in runtime from maybe ENV variable
+//    public static boolean isTest = false;
+//    private static String config = (isTest)?"test.hibernate.cfg.xml":"hibernate.cfg.xml";
 
     private static SessionFactory sessionFactory;
 
     public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            Configuration configuration = new Configuration();
-            configuration.addAnnotatedClass(Apartment.class);
-            configuration.addAnnotatedClass(Building.class);
-            configuration.addAnnotatedClass(Company.class);
-            configuration.addAnnotatedClass(Contract.class);
-            configuration.addAnnotatedClass(Employee.class);
-            configuration.addAnnotatedClass(Owner.class);
-            configuration.addAnnotatedClass(Paymentshistory.class);
-            configuration.addAnnotatedClass(Pet.class);
-            configuration.addAnnotatedClass(Resident.class);
-            configuration.addAnnotatedClass(Tax.class);
+        try {
+            if (sessionFactory == null) {
+                Configuration configuration = new Configuration();
+                configuration.configure("hibernate.cfg.xml");
+                configuration.addAnnotatedClass(Apartment.class);
+                configuration.addAnnotatedClass(Building.class);
+                configuration.addAnnotatedClass(Company.class);
+                configuration.addAnnotatedClass(Contract.class);
+                configuration.addAnnotatedClass(Employee.class);
+                configuration.addAnnotatedClass(Owner.class);
+                configuration.addAnnotatedClass(Paymentshistory.class);
+                configuration.addAnnotatedClass(Pet.class);
+                configuration.addAnnotatedClass(Resident.class);
+                configuration.addAnnotatedClass(Tax.class);
 //            configuration.addAnnotatedClass(TaxPK.class);
 
-            ServiceRegistry serviceRegistry
-                    = new StandardServiceRegistryBuilder()
-                    .applySettings(configuration.getProperties()).build();
+                ServiceRegistry serviceRegistry
+                        = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
 
-            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            }
+        }catch (Exception e) {
+            System.err.println("Initial SessionFactory creation failed." + e);
+            throw new ExceptionInInitializerError(e);
         }
 
         return sessionFactory;
